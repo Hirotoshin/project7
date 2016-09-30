7eab93ff8b34ed30d6793b94a771cc3ee1504344
Hirotoshin <hirotoshin_bsk_11@yahoo.co.jp>
2016-09-30 17:28:55 +0900

    Initial Commit

---
+++ project7/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  project7
+//
+//  Created by 伊藤潤人 on 2016/09/30.
+//  Copyright © 2016年 hirotoshin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
